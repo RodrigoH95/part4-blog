095294947c96fafcf21cc0bdfd825369e3354af6
RodrigoH95 <HRodrigo95@hotmail.com>
2022-12-28 23:38:21 -0300

    completed part 4-b

--- controllers/blog.js
+++ controllers/blog.js
@@ -9,21 +9,71 @@ blogRouter.get("/", async (request, response) => {
 blogRouter.post("/", async (request, response) => {
   const body = request.body;
 
-  if(!body || !body.title || !body.author || !body.url) {
+  if (!body || !body.title || !body.author || !body.url) {
     return response.status(400).end();
   }
 
-  if(!body.likes) body.likes = 0;
+  if (!body.likes) body.likes = 0;
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
   });
 
   const savedBlog = await blog.save();
   response.status(201).json(savedBlog);
+});
+
+blogRouter.get("/:id", async (req, res, next) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    if (blog) {
+      res.status(200).json(blog);
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(400);
+    next(err);
+  }
+});
+
+blogRouter.put("/:id", async (req, res, next) => {
+  try {
+    const body = req.body;
+
+    if (!body || !body.title || !body.author || !body.url) {
+      return res.status(400).end();
+    }
+
+    const blog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+    };
+
+    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, {
+      new: true,
+    });
+    res.json(updatedBlog);
+    console.log("Updated blog", updatedBlog);
+  } catch (err) {
+    res.status(400);
+    next(err);
+  }
+});
+
+blogRouter.delete("/:id", async (req, res, next) => {
+  try {
+    await Blog.findByIdAndDelete(req.params.id);
+    res.status(204).end();
+  } catch (err) {
+    res.status(400);
+    next(err);
+  }
 });
 
 module.exports = blogRouter;
